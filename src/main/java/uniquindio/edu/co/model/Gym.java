5f1b9e46f4ed2805e13b9313139f619e2732357a
Jacobo-Londono-Davila <jacobo.londonod@uqvirtual.edu.co>
2025-10-28 22:37:05 -0500

    modifique la clase Worker para que tenga una contraseña, entre eso se incluye el cambiar los constructores de las clases hijas, get y set y su comentacion; agregue a la clase Class la lista de entrenadores y complete la clase Gym

--- src/main/java/uniquindio/edu/co/model/Gym.java
+++ src/main/java/uniquindio/edu/co/model/Gym.java
@@ -1,4 +1,93 @@
 package uniquindio.edu.co.model;
 
+import uniquindio.edu.co.model.workers.Trainer;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Gym {
+
+    //Attributes
+    private String name;
+    private int id;
+
+    //Relationships
+    private List<User> usersList;
+    private List<Trainer> trainersList;
+
+    /**
+     * Gym class Constructor
+     * @param name of the Gym
+     * @param id of the Gym
+     */
+    public Gym(String name, int id) {
+        this.name = name;
+        this.id = id;
+        this.usersList = new ArrayList<>();
+        this.trainersList = new ArrayList<>();
+    }
+
+    /**
+     * Get gym name
+     * @return name of the gym
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Set gym name
+     * @param name of the gym
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Get gym id
+     * @return gym id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * Set gym id
+     * @param id if the gym
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * Get the list of users in the gym
+     * @return gym usersList
+     */
+    public List<User> getUsersList() {
+        return usersList;
+    }
+
+    /**
+     * Set the list of users in the gym
+     * @param usersList of the gym
+     */
+    public void setUsersList(List<User> usersList) {
+        this.usersList = usersList;
+    }
+
+    /**
+     * Get the list of trainers in the gym
+     * @return gym trainersList
+     */
+    public List<Trainer> getTrainersList() {
+        return trainersList;
+    }
+
+    /**
+     * Set the list of trainers in the gym
+     * @param trainersList of the gym
+     */
+    public void setTrainersList(List<Trainer> trainersList) {
+        this.trainersList = trainersList;
+    }
 }
--- src/main/java/uniquindio/edu/co/model/Student.java
+++ src/main/java/uniquindio/edu/co/model/Student.java
@@ -1,5 +1,17 @@
 package uniquindio.edu.co.model;
 
-public class Student {
-
+public class Student extends User {
+    /**
+     * Student class Constructor
+     *
+     * @param name          of the Student
+     * @param lastName      of the Student
+     * @param personalId    of the Student
+     * @param phoneNumber   of the Student
+     * @param age           of the Student
+     * @param theMembership of the Student
+     */
+    public Student(String name, String lastName, int personalId, String phoneNumber, int age, Membership theMembership) {
+        super(name, lastName, personalId, phoneNumber, age, theMembership);
+    }
 }
